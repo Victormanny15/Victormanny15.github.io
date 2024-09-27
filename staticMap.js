1520211b044bcf7d4ab41925122cada239b1be56
Victormanny15 <1300victorm@gmail.com>
2024-09-27 09:33:56 -0600

    commit 4

--- staticMap.js
+++ staticMap.js
@@ -1,11 +1,26 @@
+// Initialize and add the map
 let map;
 
 async function initMap() {
+  // The location of Uluru
+  const position = { lat: 21.814356279590402, lng: -102.76975792659823 };
+  // Request needed libraries.
+  //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
+  // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    zoom: 17,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
   });
 }
 
